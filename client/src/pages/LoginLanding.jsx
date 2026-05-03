78e6e4909c8c635381ceeab70b64451f3a5f4e4a
kashifashah1 <kashifa.shah24@gmail.com>
2026-05-03 16:00:48 +0530

    Pages and Login component added

---
+++ client/src/pages/LoginLanding.jsx
@@ -0,0 +1,25 @@
+import LoginLeftSide from "../components/LoginLeftSide"
+
+const LoginLanding = () => {
+    return (
+        <div className="min-h-screen flex flex-col md:flex-row">
+            <LoginLeftSide />
+
+            <div className="w-full md:w-1/2 flex flex-col items-center justify-center p-6 sm:p-12 lg:p-16 relative overflow-y-auto min-h-screen">
+            
+            <div className="w-full max-w-md animate-fade-in relative z-10">
+                {/* Header */}
+
+                {/* Portal List */}
+
+                {/* Footer */}
+            </div>
+
+            </div>
+        </div>
+
+
+    )
+}
+
+export default LoginLanding
